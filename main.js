ae08d869ce156f78fd0c5eba72d50f57c0410e89
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2023-12-18 11:34:50 -0600

    completed

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 function numberDoubler(num) {
-    // Your code here
+while(num < 100){
+  num *= 2
+}
+return num
 }
 
 function stringRepeater(str) {
-    // Your code here
+let str2 = ""
+while(str2.length < 10){
+  str2 = str + str2
+}
+return str2
 }
 
+
 function makeDivisible(x, y) {
-    // Your code here
+
+while(x % y !== 0){
+    x = x + 1
+}
+return x
 }
 
 module.exports = {
